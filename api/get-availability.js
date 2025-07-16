a890fdf1c5683143c619203fa10ffa4de6b7e481
Russell-TK-Martin <russell.martin@team-sport.com>
2025-07-16 18:44:41 +0100

    Update get-availability.js
    Updating parameters passed to webhook

--- api/get-availability.js
+++ api/get-availability.js
@@ -5,14 +5,14 @@ module.exports = async (req, res) => {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const { activity, date } = req.body;
+  const { activity, date, chatInput, mysessionId } = req.body;
 
   try {
     console.log("Calling n8n webhook with:", { activity, date });
 
     const response = await axios.post(
       "https://teamsport.app.n8n.cloud/webhook-test/64dc8ba7-3d5b-4516-b708-9e74c8c6f08a",
-      { activity, date },
+      { activity, date, chatInput, mysessionId },
       { headers: { "Content-Type": "application/json" } }
     );
 
