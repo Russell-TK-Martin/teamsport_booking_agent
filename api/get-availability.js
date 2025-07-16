f8b7cdce7469a9444d145aa9158474d6cb1fbc18
Russell-TK-Martin <russell.martin@team-sport.com>
2025-07-16 17:09:41 +0100

    Update get-availability.js
    Updating to Aplha testing

--- api/get-availability.js
+++ api/get-availability.js
@@ -11,7 +11,7 @@ module.exports = async (req, res) => {
     console.log("Calling n8n webhook with:", { activity, date });
 
     const response = await axios.post(
-      "https://teamsport.app.n8n.cloud/webhook-test/965713ca-2fae-4c07-8532-e87865f70d3a",
+      "https://teamsport.app.n8n.cloud/webhook-test/64dc8ba7-3d5b-4516-b708-9e74c8c6f08a",
       { activity, date },
       { headers: { "Content-Type": "application/json" } }
     );
